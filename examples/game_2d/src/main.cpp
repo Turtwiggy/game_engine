11abca4bbb12fc6c0a0624ef5f9d00f1ab172c46
Turtwiggy <m-p-b@hotmail.co.uk>
2021-02-28 21:07:14 +0000

    drawing lines

--- examples/game_2d/src/main.cpp
+++ examples/game_2d/src/main.cpp
@@ -22,7 +22,6 @@ using namespace fightingengine;
 
 // game headers
 #include "camera2d.hpp"
-#include "game.hpp"
 #include "sprite_renderer.hpp"
 #include "spritemap.hpp"
 #include "util.hpp"
@@ -38,28 +37,28 @@ using namespace game2d;
 // https://github.com/Turtwiggy/Dwarf-and-Blade/blob/master/src/sprite_renderer.cpp
 // https://github.com/Turtwiggy/Dwarf-and-Blade/tree/master/src
 
-struct Transform
-{
-  glm::vec2 position = { 0.0f, 0.0f }; // in pixels, centered
-  float angle = 0.0f;                  // in degrees
-  glm::vec2 scale = { 100.0f, 100.0f };
-  glm::vec4 colour = { 1.0f, 1.0f, 1.0f, 1.0f };
-};
-
 struct GameObject
 {
-  Transform transform;
+  glm::vec2 pos = { 0.0f, 0.0f }; // in pixels, centered
+  float angle = 0.0f;             // in degrees
+  glm::vec2 size = { 100.0f, 100.0f };
+  glm::vec4 colour = { 1.0f, 1.0f, 1.0f, 1.0f };
   glm::vec2 velocity = { 0.0f, 0.0f };
+
   int tex_slot = 0;
-
-  // gameobject flags...
-  // note: avoid adding these, or come up with a better system
-  // more flags is a 2^n of configurations of testing to make sure everything
-  bool is_solid = false;
-  bool destroyed = false;
-
-  GameObject() = default;
 };
+
+// simple aabb collision
+bool
+check_collides(GameObject& one, GameObject& two)
+{
+  // collision x-axis?
+  bool collisionX = one.pos.x + one.size.x >= two.pos.x && two.pos.x + two.size.x >= one.pos.x;
+  // collision y-axis?
+  bool collisionY = one.pos.y + one.size.y >= two.pos.y && two.pos.y + two.size.y >= one.pos.y;
+  // collision only if on both axes
+  return collisionX && collisionY;
+}
 
 enum class GameState
 {
@@ -72,12 +71,9 @@ enum class GameState
 
 float screen_width = 1366.0f;
 float screen_height = 768.0f;
-
 const glm::vec4 dark_blue = glm::vec4(0.0f / 255.0f, 100.0f / 255.0f, 100.0f / 255.0f, 1.0f);
-const int tex_unit_kenny_nl = 0;
 
-const float player_speed = 10.0f;
-const float camera_speed = 100.0f;
+const int tex_unit_kenny_nl = 0;
 
 int
 main()
@@ -112,15 +108,18 @@ main()
   RenderCommand::set_viewport(0, 0, screen_width, screen_height);
   RenderCommand::set_depth_testing(false); // disable depth testing for 2d
 
-  Shader sprite_shader = Shader("2d_texture.vert", "2d_spritesheet.frag");
+  Shader sprite_shader = Shader("2d.vert", "2d_spritesheet.frag");
   sprite_shader.bind();
   sprite_shader.set_mat4("projection", projection);
   sprite_shader.set_int("tex", tex_unit_kenny_nl);
 
-  Shader tex_shader = Shader("2d_texture.vert", "2d_texture.frag");
+  Shader tex_shader = Shader("2d.vert", "2d_texture.frag");
   tex_shader.bind();
   tex_shader.set_mat4("projection", projection);
   tex_shader.set_int("tex", tex_unit_kenny_nl);
+
+  Shader red_shader = Shader("2d_red.vert", "2d_red.frag");
+  red_shader.bind();
 
   sprite::spritemap spritemap;
   auto& sprites = spritemap.get_locations();
@@ -128,28 +127,25 @@ main()
   // ---- Game ----
 
   GameState state = GameState::GAME_ACTIVE;
-  std::vector<glm::vec2> objects;
+  std::vector<GameObject> objects;
+  float spawn_every = 1.0f;
+  float spawn_every_cooldown = 0.0f;
 
   GameObject player;
-  player.transform.angle = 90.0f;
-  player.transform.colour = { 0.0f, 1.0f, 0.0f, 0.6f };
-  player.transform.scale = { 1.0f * 768.0f / 48.0f, 1.0f * 362.0f / 22.0f };
-  player.transform.position = { screen_width / 2.0f, screen_height / 2.0f };
+  player.angle = 90.0f;
+  player.colour = { 0.0f, 1.0f, 0.0f, 0.6f };
+  player.size = { 1.0f * 768.0f / 48.0f, 1.0f * 362.0f / 22.0f };
+  player.pos = { screen_width / 2.0f, screen_height / 2.0f };
   player.tex_slot = tex_unit_kenny_nl;
   player.velocity = { 5.0f, 5.0f };
 
   // player things
-  float boop_cooldown = 1.0f;
-  bool l_boop = false;
-  float l_boop_cooldown_left = 1.0f;
-  bool r_boop = false;
-  float r_boop_cooldown_left = 1.0f;
 
   GameObject tex_obj;
-  tex_obj.transform.angle = 0.0f;
-  tex_obj.transform.colour = { 1.0f, 1.0f, 1.0f, 1.0f };
-  tex_obj.transform.scale = { 768.0f, 352.0f };
-  tex_obj.transform.position = { 0.0f, 20.0f };
+  tex_obj.angle = 0.0f;
+  tex_obj.colour = { 1.0f, 1.0f, 1.0f, 1.0f };
+  tex_obj.size = { 768.0f, 352.0f };
+  tex_obj.pos = { 0.0f, 20.0f };
   tex_obj.tex_slot = tex_unit_kenny_nl;
   tex_obj.velocity = { 0.0f, 0.0f };
 
@@ -164,20 +160,39 @@ main()
 
   float percent = 0.0f;
   GameObject floaty_object;
-  floaty_object.transform.position = points[0];
+  floaty_object.pos = points[0];
   glm::vec2 floaty_object_destination = points[1];
   floaty_object.tex_slot = tex_unit_kenny_nl;
 
-  // levels
+  // https://solarianprogrammer.com/2013/05/13/opengl-101-drawing-primitives/
+  // https://www.youtube.com/watch?v=D2a5fHX-Qrs&t=2s circle vs rectangle
+  // aabb https://learnopengl.com/In-Practice/2D-Game/Collisions/Collision-detection
+  // https://www.youtube.com/watch?v=NbSee-XM7WA ray casting using dda
 
-  // breakout.levels.clear();
-  // GameLevel level_0;
-  // {
-  //   std::vector<std::vector<int>> level_0_bricks;
-  //   load_level_from_file(level_0_bricks, "assets/breakout/level_0.breakout");
-  //   init_level(level_0, level_0_bricks, screen_width, static_cast<int>(screen_height / 2.0f));
-  // }
-  // breakout.levels.push_back(level_0);
+  // clang-format off
+  GLfloat line_verts[] = 
+  { 
+    -screen_width, -screen_height,
+    screen_width, screen_height,
+    -screen_width, screen_height,
+    screen_width, -screen_height,
+  };
+  // clang-format on
+  unsigned int line_vao = 0;
+  unsigned int line_vbo;
+  // bind
+  glGenVertexArrays(1, &line_vao);
+  glGenBuffers(1, &line_vbo);
+  // load data vbo
+  glBindBuffer(GL_ARRAY_BUFFER, line_vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(line_verts), line_verts, GL_STATIC_DRAW);
+  // load data vao
+  glBindVertexArray(line_vao);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+  // unbind
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
 
   // ---- App ----
 
@@ -212,6 +227,8 @@ main()
       sprite_shader.set_mat4("projection", projection);
       tex_shader.bind();
       tex_shader.set_mat4("projection", projection);
+      red_shader.bind();
+      red_shader.set_mat4("projection", projection);
     }
 
     //
@@ -226,49 +243,52 @@ main()
       sprite_shader.set_int("desired_x", 0);
     }
 
-    // Game Thing: Pause
+    //
+    // Game: Pause
+    //
     if (app.get_input().get_key_down(SDL_SCANCODE_P)) {
-      state = GameState::GAME_PAUSED;
+      state = state == GameState::GAME_PAUSED ? GameState::GAME_ACTIVE : GameState::GAME_PAUSED;
     }
-    if (app.get_input().get_key_up(SDL_SCANCODE_P)) {
-      state = GameState::GAME_ACTIVE;
-    }
-
-    // Game Thing: Reset player pos
-    if (app.get_input().get_key_held(SDL_SCANCODE_O)) {
-      player.transform.position = glm::vec2(0.0f, 0.0f);
-    }
-
-    // Reset Camera
-    // if (app.get_input().get_key_held(SDL_SCANCODE_E))
-    camera.pos =
-      glm::vec2(player.transform.position.x - screen_width / 2.0f, player.transform.position.y - screen_height / 2.0f);
 
     //
-    // Editor: Add object
+    // Add object
     //
     if (app.get_input().get_mouse_lmb_down()) {
       glm::ivec2 mouse_pos = app.get_input().get_mouse_pos();
       printf("(game) left mouse clicked %i %i \n", mouse_pos.x, mouse_pos.y);
-
       glm::vec2 world_pos = glm::vec2(mouse_pos) + camera.pos;
-
-      objects.push_back(glm::vec2(world_pos.x, world_pos.y));
+      GameObject obj;
+      obj.pos = glm::vec2(world_pos.x, world_pos.y);
+      objects.push_back(obj);
     }
-
-    // if (app.get_input().get_key_down(SDL_KeyCode::SDLK_t))
-    //   dynamic_body->SetTransform(b2Vec2(0.0f, 0.0f), dynamic_body->GetAngle());
-
-    // if (app.get_input().get_mouse_lmb_held()) {
-    //   glm::ivec2 mouse_pos = app.get_window().get_mouse_position();
-    //   dynamic_body->SetTransform(b2Vec2(mouse_pos.x, mouse_pos.y), dynamic_body->GetAngle());
-    // }
 
     profiler.end(Profiler::Stage::SdlInput);
     profiler.begin(Profiler::Stage::GameTick);
 
     if (state == GameState::GAME_ACTIVE) {
 
+      // Game Thing: Randomly spawn things
+      spawn_every_cooldown += delta_time_s;
+      if (spawn_every_cooldown > spawn_every) {
+
+        float x = rand_det_s(rnd.rng, 0.0f, 1.0f);
+        float y = rand_det_s(rnd.rng, 0.0f, 1.0f);
+        glm::vec2 rand_pos(x * screen_width, y * screen_height);
+        printf("random pos: %f %f", rand_pos.x, rand_pos.y);
+
+        GameObject obj;
+        obj.pos = rand_pos;
+        objects.push_back(obj);
+
+        spawn_every_cooldown = 0.0f;
+      }
+
+      // Game Thing: Reset player pos
+      if (app.get_input().get_key_held(SDL_SCANCODE_O)) {
+        player.pos = glm::vec2(0.0f, 0.0f);
+      }
+
+      const float camera_speed = 100.0f;
       float camera_velocity_x = delta_time_s * camera_speed;
       float camera_velocity_y = delta_time_s * camera_speed;
 
@@ -283,8 +303,8 @@ main()
         camera.pos.y += camera_velocity_y;
 
       // spaceship lrud
+      const float player_speed = 10.0f;
 
-      float angle_speed = offsets;
       if (app.get_input().get_key_held(SDL_SCANCODE_W))
         player.velocity.y += player_speed;
       if (app.get_input().get_key_held(SDL_SCANCODE_S))
@@ -293,62 +313,36 @@ main()
       float turn_velocity_x = player.velocity.y; // same as y so turning doesnt feel weird
       float turn_velocity_y = player.velocity.y;
 
-      // if (app.get_input().get_key_held(SDL_SCANCODE_SPACE))
-      //   turn_velocity_x /= 2.0f;
-
+      float angle_speed = 200.0f;
       if (app.get_input().get_key_held(SDL_SCANCODE_SPACE)) {
-        // turn_velocity_x = 0.0f;
-        turn_velocity_y *= 0.1f;
-        turn_velocity_x *= 0.1f;
+        turn_velocity_x = 0.0f;
+        turn_velocity_y = 0.0f;
+        // turn_velocity_y *= 0.1f;
+        // turn_velocity_x *= 0.1f;
         // increase turn speed
         angle_speed *= 5.0f;
       }
 
       // Turn Ship
       if (app.get_input().get_key_held(SDL_SCANCODE_D))
-        player.transform.angle += delta_time_s * angle_speed;
+        player.angle += delta_time_s * angle_speed;
       if (app.get_input().get_key_held(SDL_SCANCODE_A))
-        player.transform.angle -= delta_time_s * angle_speed;
+        player.angle -= delta_time_s * angle_speed;
 
-      float extra_x = 0.0f;
-      float extra_y = 0.0f;
-      float boop_amount = 5000.0f;
-
-      if (app.get_input().get_key_down(SDL_SCANCODE_A) && l_boop) {
-        l_boop = false;
-        extra_x = glm::sin(glm::radians(player.transform.angle - 90.0f)) * boop_amount;
-        extra_y = -glm::cos(glm::radians(player.transform.angle - 90.0f)) * boop_amount;
-      } else if (app.get_input().get_key_down(SDL_SCANCODE_A)) {
-        l_boop = true;
-        l_boop_cooldown_left = boop_cooldown;
-      }
-      if (l_boop_cooldown_left > 0.0f)
-        l_boop_cooldown_left -= delta_time_s;
-      else
-        l_boop = false;
-
-      if (app.get_input().get_key_down(SDL_SCANCODE_D) && r_boop) {
-        r_boop = false;
-        extra_x = glm::sin(glm::radians(player.transform.angle + 90.0f)) * boop_amount;
-        extra_y = -glm::cos(glm::radians(player.transform.angle + 90.0f)) * boop_amount;
-      } else if (app.get_input().get_key_down(SDL_SCANCODE_D)) {
-        r_boop = true;
-        r_boop_cooldown_left = boop_cooldown;
-      }
-      if (r_boop_cooldown_left > 0.0f)
-        r_boop_cooldown_left -= delta_time_s;
-      else
-        r_boop = false;
+      // Boost
+      float extra_speed = 1.0f;
+      if (app.get_input().get_key_held(SDL_SCANCODE_LSHIFT))
+        extra_speed = 2.0f;
 
       // update get vector based on angle
-      float x = glm::sin(glm::radians(player.transform.angle)) * turn_velocity_x;
-      float y = -glm::cos(glm::radians(player.transform.angle)) * turn_velocity_y;
+      float x = glm::sin(glm::radians(player.angle)) * turn_velocity_x * extra_speed;
+      float y = -glm::cos(glm::radians(player.angle)) * turn_velocity_y * extra_speed;
 
-      player.transform.position.x += (x + extra_x) * delta_time_s;
-      player.transform.position.y += (y + extra_y) * delta_time_s;
+      player.pos.x += x * delta_time_s;
+      player.pos.y += y * delta_time_s;
 
       // move floaty object to point
-      floaty_object.transform.position = glm::lerp(points[next_point], points[(next_point + 1) % 4], percent);
+      floaty_object.pos = glm::lerp(points[next_point], points[(next_point + 1) % 4], percent);
       float time_to_take_seconds = 4.0f;
       percent += delta_time_s / time_to_take_seconds;
       if (percent >= 1.0f) {
@@ -356,6 +350,35 @@ main()
         next_point += 1;
         next_point %= 4;
       }
+
+      // Shoot
+      if (app.get_input().get_mouse_rmb_down()) {
+        glm::ivec2 mouse_pos = app.get_input().get_mouse_pos();
+        printf("(game) right mouse clicked %i %i \n", mouse_pos.x, mouse_pos.y);
+        glm::vec2 world_pos = glm::vec2(mouse_pos) + camera.pos;
+
+        GameObject obj;
+        obj.pos = player.pos;
+        obj.angle = player.angle;
+        obj.velocity = player.velocity;
+        objects.push_back(obj);
+      }
+
+      // Do Collisions
+      std::vector<GameObject>::iterator it = objects.begin();
+      while (it != objects.end()) {
+
+        if (check_collides(player, *it)) {
+          printf("Ahh! you are colliding with something");
+          it = objects.erase(it);
+        } else {
+          ++it;
+        }
+      }
+
+      // Reset Camera
+      if (app.get_input().get_key_held(SDL_SCANCODE_Q))
+        camera.pos = glm::vec2(player.pos.x - screen_width / 2.0f, player.pos.y - screen_height / 2.0f);
     }
 
     profiler.end(Profiler::Stage::GameTick);
@@ -366,25 +389,32 @@ main()
     RenderCommand::set_clear_colour(dark_blue);
     RenderCommand::clear();
 
-    tex_shader.bind();
-    sprite_renderer::draw_sprite(camera,
-                                 glm::ivec2(screen_width, screen_height),
-                                 tex_shader,
-                                 tex_obj.transform.position,
-                                 tex_obj.transform.scale,
-                                 tex_obj.transform.angle,
-                                 tex_obj.transform.colour);
+    // tex_shader.bind();
+    // sprite_renderer::draw_sprite(camera,
+    //                              glm::ivec2(screen_width, screen_height),
+    //                              tex_shader,
+    //                              tex_obj.pos,
+    //                              tex_obj.size,
+    //                              tex_obj.angle,
+    //                              tex_obj.colour);
 
     glm::ivec2 obj = spritemap.get_sprite_offset(sprite::type::TREE_1);
     sprite_shader.bind();
     sprite_shader.set_int("desired_x", obj.x);
     sprite_shader.set_int("desired_y", obj.y);
 
+    float extra_speed = 10.0f;
     for (auto& object : objects) {
       glm::vec2 size = glm::vec2(50.0f, 50.0f);
       glm::vec3 colour = glm::vec3(1.0f, 0.0f, 0.0f);
+
+      float x = glm::sin(glm::radians(object.angle)) * object.velocity.x;
+      float y = -glm::cos(glm::radians(object.angle)) * object.velocity.y;
+      object.pos.x += (x)*delta_time_s;
+      object.pos.y += (y)*delta_time_s;
+
       sprite_renderer::draw_sprite(
-        camera, glm::ivec2(screen_width, screen_height), sprite_shader, object, size, 0.0f, colour);
+        camera, glm::ivec2(screen_width, screen_height), sprite_shader, object.pos, size, object.angle, colour);
     }
     for (auto& object : points) {
       glm::vec2 size = glm::vec2(50.0f, 50.0f);
@@ -396,10 +426,10 @@ main()
     sprite_renderer::draw_sprite(camera,
                                  glm::ivec2(screen_width, screen_height),
                                  sprite_shader,
-                                 floaty_object.transform.position,
-                                 floaty_object.transform.scale,
-                                 floaty_object.transform.angle,
-                                 floaty_object.transform.colour);
+                                 floaty_object.pos,
+                                 floaty_object.size,
+                                 floaty_object.angle,
+                                 floaty_object.colour);
 
     obj = spritemap.get_sprite_offset(sprite::type::TREE_1);
     sprite_shader.set_int("desired_x", obj.x);
@@ -407,10 +437,16 @@ main()
     sprite_renderer::draw_sprite(camera,
                                  glm::ivec2(screen_width, screen_height),
                                  sprite_shader,
-                                 player.transform.position,
-                                 player.transform.scale,
-                                 player.transform.angle,
-                                 player.transform.colour);
+                                 player.pos,
+                                 player.size,
+                                 player.angle,
+                                 player.colour);
+
+    // draw a red line
+    red_shader.bind();
+    glBindVertexArray(line_vao);
+    glDrawArrays(GL_LINES, 0, 4);
+    glBindVertexArray(0);
 
     profiler.end(Profiler::Stage::Render);
     profiler.begin(Profiler::Stage::GuiLoop);
@@ -430,12 +466,13 @@ main()
     }
 
     ImGui::Begin("Game Info", NULL, ImGuiWindowFlags_NoFocusOnAppearing);
-    ImGui::Text("player pos %f %f", player.transform.position.x, player.transform.position.y);
-    ImGui::Text("player vel x: %f y: %f", player.velocity.x, player.velocity.y);
-    ImGui::Text("player angle %f", player.transform.angle);
+    ImGui::Text("player pos %f %f", player.pos.x, player.pos.y);
+    ImGui::Text("player accel x: %f y: %f", player.velocity.x, player.velocity.y);
+    ImGui::Text("player vel (BROKEN) x: %f y: %f", 0.0f, 0.0f);
+    ImGui::Text("player angle %f", player.angle);
     ImGui::Text("camera pos %f %f", camera.pos.x, camera.pos.y);
     ImGui::Text("mouse pos %f %f", app.get_input().get_mouse_pos().x, app.get_input().get_mouse_pos().y);
-    ImGui::Text("lboop %d rboop %d", l_boop, r_boop);
+    ImGui::Text("Spawned objects: %i", objects.size());
     ImGui::End();
 
     // ImGui demo window
@@ -454,3 +491,44 @@ main()
     profiler.end(Profiler::Stage::UpdateLoop);
   }
 }
+
+// CODE GRAVEYARD
+
+//
+// Boop left or right feature
+//
+
+// float boop_cooldown = 1.0f;
+// bool l_boop = false;
+// float l_boop_cooldown_left = 1.0f;
+// bool r_boop = false;
+// float r_boop_cooldown_left = 1.0f;
+
+// float extra_x = 0.0f;
+// float extra_y = 0.0f;
+// // float boop_amount = 5000.0f;
+// // if (app.get_input().get_key_down(SDL_SCANCODE_A) && l_boop) {
+// //   l_boop = false;
+// //   extra_x = glm::sin(glm::radians(player.angle - 90.0f)) * boop_amount;
+// //   extra_y = -glm::cos(glm::radians(player.angle - 90.0f)) * boop_amount;
+// // } else if (app.get_input().get_key_down(SDL_SCANCODE_A)) {
+// //   l_boop = true;
+// //   l_boop_cooldown_left = boop_cooldown;
+// // }
+// // if (l_boop_cooldown_left > 0.0f)
+// //   l_boop_cooldown_left -= delta_time_s;
+// // else
+// //   l_boop = false;
+
+// // if (app.get_input().get_key_down(SDL_SCANCODE_D) && r_boop) {
+// //   r_boop = false;
+// //   extra_x = glm::sin(glm::radians(player.angle + 90.0f)) * boop_amount;
+// //   extra_y = -glm::cos(glm::radians(player.angle + 90.0f)) * boop_amount;
+// // } else if (app.get_input().get_key_down(SDL_SCANCODE_D)) {
+// //   r_boop = true;
+// //   r_boop_cooldown_left = boop_cooldown;
+// // }
+// // if (r_boop_cooldown_left > 0.0f)
+// //   r_boop_cooldown_left -= delta_time_s;
+// // else
+// //   r_boop = false;
