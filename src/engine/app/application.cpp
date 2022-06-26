8b278e5473fc5f2b65ee56b0a18b533bf544ee7f
Turtwiggy <m-p-b@hotmail.co.uk>
2022-06-26 14:38:25 +0100

    rip out input

--- src/engine/app/application.cpp
+++ src/engine/app/application.cpp
@@ -3,8 +3,6 @@
 #include "engine/app/application.hpp"
 
 // other lib headers
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_events.h>
 #include <imgui.h>
 
 // c++ standard lib headers
@@ -58,95 +56,13 @@ Application::get_frame()
 }
 
 void
-Application::poll_input()
-{
-  SDL_Event e;
-  while (SDL_PollEvent(&e)) {
-
-    // Events to quit
-    if (e.type == SDL_QUIT) {
-      on_window_close();
-    }
-
-    // https://wiki.libsdl.org/SDL_WindowEvent
-    if (e.type == SDL_WINDOWEVENT) {
-
-      switch (e.window.event) {
-        case SDL_WINDOWEVENT_CLOSE:
-          if (e.window.windowID == SDL_GetWindowID(window->get_handle())) {
-            on_window_close();
-          }
-          break;
-        case SDL_WINDOWEVENT_RESIZED:
-          if (e.window.windowID == SDL_GetWindowID(window->get_handle())) {
-            on_window_resize(e.window.data1, e.window.data2);
-          }
-          break;
-        case SDL_WINDOWEVENT_MINIMIZED:
-          if (e.window.windowID == SDL_GetWindowID(window->get_handle())) {
-            minimized = true;
-          }
-          break;
-        case SDL_WINDOWEVENT_FOCUS_GAINED:
-          SDL_Log("Window %d gained keyboard focus \n", e.window.windowID);
-          break;
-        case SDL_WINDOWEVENT_FOCUS_LOST:
-          SDL_Log("Window %d lost keyboard focus \n", e.window.windowID);
-          break;
-      }
-    }
-
-    if (ImGui::GetIO().WantCaptureMouse || ImGui::GetIO().WantCaptureKeyboard) {
-      imgui_manager.process_event(&e);
-      // continue; // Imgui stole the event
-    }
-
-    // audio
-    if (e.type == SDL_AUDIODEVICEADDED)
-      std::cout << "audio device added?" << std::endl;
-    if (e.type == SDL_AUDIODEVICEREMOVED)
-      std::cout << "audio device removed?" << std::endl;
-
-    // keyboard specific
-    if (e.type == SDL_KEYDOWN)
-      input_manager.process_key_down(e.key.keysym.scancode, e.key.repeat);
-    if (e.type == SDL_KEYUP)
-      input_manager.process_key_up(e.key.keysym.scancode, e.key.repeat);
-
-    // controller specific
-    // if (e.type == SDL_JOYHATMOTION)
-    //   input_manager.process_controller_dpad(e.jhat);
-    // if (e.type == SDL_JOYBUTTONDOWN)
-    //   input_manager.process_controller_button_down(e.jbutton);
-    // if (e.type == SDL_JOYBUTTONUP)
-    //   input_manager.process_controller_button_up(e.jbutton);
-    if (e.type == SDL_JOYDEVICEADDED)
-      input_manager.process_controller_added();
-    if (e.type == SDL_JOYDEVICEREMOVED)
-      input_manager.process_controller_removed();
-
-    // mouse specific
-    if (e.type == SDL_MOUSEBUTTONDOWN) {
-      // input_manager uses ImGui for mouse events
-      // input_manager.process_mouse_event(e.button);
-    }
-    // mouse scrollwheel
-    if (e.type == SDL_MOUSEWHEEL) {
-      input_manager.set_mousewheel_y(static_cast<float>(e.wheel.y));
-    }
-  }
-}
-
-void
 Application::frame_begin()
 {
   frame += 1;
   if (frame == std::numeric_limits<uint64_t>::max())
     frame = 0;
 
-  input_manager.new_frame(frame);
-
-  imgui_manager.begin_frame(get_window());
+  imgui_manager.begin_frame(*window);
 
   seconds_since_launch += get_delta_time();
 }
@@ -154,9 +70,9 @@ Application::frame_begin()
 void
 Application::frame_end(Uint64& frame_start_time)
 {
-  imgui_manager.end_frame(get_window());
+  imgui_manager.end_frame(*window);
 
-  SDL_GL_SwapWindow(get_window().get_handle());
+  SDL_GL_SwapWindow(window->get_handle());
 
   Uint64 frame_end_time = SDL_GetPerformanceCounter();
   float elapsed_ms = (frame_end_time - frame_start_time) / static_cast<float>(SDL_GetPerformanceFrequency()) * 1000.0f;
@@ -166,43 +82,6 @@ Application::frame_end(Uint64& frame_start_time)
     if (delay > 0.0f)
       SDL_Delay(static_cast<Uint32>(delay));
   }
-}
-
-// ---- events
-
-void
-Application::on_window_close()
-{
-  printf("application close event recieved");
-  running = false;
-}
-
-void
-Application::on_window_resize(int new_w, int new_h)
-{
-  printf("application resize event recieved: %i %i ", new_w, new_h);
-  window_was_resized = true;
-
-  if (new_w == 0 || new_h == 0) {
-    minimized = true;
-    return;
-  }
-
-  minimized = false;
-}
-
-// ---- window controls
-
-InputManager&
-Application::get_input()
-{
-  return input_manager;
-}
-
-GameWindow&
-Application::get_window()
-{
-  return *window;
 }
 
 }
--- src/engine/app/application.hpp
+++ src/engine/app/application.hpp
@@ -4,7 +4,6 @@
 #include "engine/app/game_window.hpp"
 
 // engine headers
-#include "engine/app/input.hpp"
 #include "engine/app/setup_imgui.hpp"
 
 // c++ standard library headers
@@ -27,7 +26,6 @@ public:
   void shutdown();
   [[nodiscard]] float get_delta_time();
 
-  void poll_input();
   void frame_begin();
   void frame_end(Uint64& frame_start_time);
   uint64_t get_frame();
@@ -36,41 +34,13 @@ public:
   float seconds_since_launch = 0.0f;
   float fps_if_limited = 60.0f;
   bool limit_fps = false;
-  bool window_was_resized = false;
-
-  [[nodiscard]] GameWindow& get_window();
-  [[nodiscard]] InputManager& get_input();
-
-private:
-  void on_window_close();
-  void on_window_resize(int new_w, int new_h);
 
 private:
   std::unique_ptr<GameWindow> window;
-  InputManager input_manager;
   ImGui_Manager imgui_manager;
 
   uint64_t frame = 0;
   bool running = true;
   bool minimized = false;
-
-  // Game's fixed tick
-  // int FIXED_TICKS_PER_SECOND = 1;
-  // float SECONDS_PER_FIXED_TICK = 1.0f / FIXED_TICKS_PER_SECOND;
-  // float seconds_since_last_game_tick = 0;
-
-  // Physics tick
-  // int PHYSICS_TICKS_PER_SECOND = 60;
-  // float SECONDS_PER_PHYSICS_TICK = 1.0f / PHYSICS_TICKS_PER_SECOND;
 };
 }
-
-// THIS IS FOR A FIXED GAME TICK
-// -----------------------------
-// seconds_since_last_game_tick += delta_time_in_seconds;
-// while (seconds_since_last_game_tick >= SECONDS_PER_FIXED_TICK)
-//{
-//    //Fixed update
-//    fixed_tick(SECONDS_PER_FIXED_TICK);
-//    seconds_since_last_game_tick -= SECONDS_PER_FIXED_TICK;
-//}
