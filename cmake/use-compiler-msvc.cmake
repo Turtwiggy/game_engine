fe569287dc235a01300c86b4b0f3ce476cda2d1f
Turtwiggy <m-p-b@hotmail.co.uk>
2025-01-29 16:48:02 +0000

    [github actions] warnings as errors only for debug

--- cmake/use-compiler-msvc.cmake
+++ cmake/use-compiler-msvc.cmake
@@ -3,7 +3,10 @@ if(CMAKE_CXX_COMPILER_ID MATCHES MSVC)
   # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /d1reportTime") # debug compiler frontend???
   # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /d2cgsummary") # debug compiler backend???
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /MP /fp:precise")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX") # warnings as errors
+
+  if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /WX") # warnings as errors
+  endif()
 
   # set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3 /external:I ${CMAKE_SOURCE_DIR}/thirdparty")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /W3 /wd4275 /wd4251 /bigobj")
